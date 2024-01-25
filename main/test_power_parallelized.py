ad3f4aff4f41508e905ce34879e435da3a8d1e58
Yufeng Wu <sw20@williams.edu>
2024-01-25 16:08:36 -0500

    test for brownswiss

--- main/test_power_parallelized.py
+++ main/test_power_parallelized.py
@@ -64,7 +64,7 @@ def process_iteration(params):
     }
 
 def main():
-    true_models = ["BBU", "UBU", "UBB", "BBB"]
+    true_models = ["UBB", "BBB"] #["BBU", "UBU", "UBB", "BBB"]
     effective_sample_sizes = [500, 1000, 2000, 3000, 4000, 5000]
 
     columns = ['true_model', 'data_source', 'network_size', 'effective_sample_size',
