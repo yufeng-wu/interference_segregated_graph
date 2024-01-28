fee3d110ce30da2b350b53da8f90556b887648b3
Yufeng Wu <sw20@williams.edu>
2024-01-28 00:17:27 -0500

    testing decision tree

--- main/UBB_test_power_parallelized_5000.py
+++ main/UBB_test_power_parallelized_5000.py
@@ -7,8 +7,10 @@ from datetime import datetime
 from nonparametric_test_undirected_vs_bidirected import prepare_data, test_edge_type
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.neighbors import KNeighborsRegressor
 import warnings
 from joblib import parallel_backend
+from sklearn.tree import DecisionTreeRegressor
 
 # Filter out the UserWarning related to nested parallelism
 warnings.filterwarnings('ignore', category=UserWarning, message='.*Loky-backed parallel loops cannot be called in a multiprocessing.*')
@@ -16,7 +18,7 @@ warnings.filterwarnings('ignore', category=UserWarning, message='.*Loky-backed p
 # Global variables
 timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 FOLDER_TO_SAVE = "../result/"
-FILENAME_TO_SAVE = FOLDER_TO_SAVE + f"UBB_FINAL_TEST_RESULT_{timestamp}.csv"
+FILENAME_TO_SAVE = FOLDER_TO_SAVE + f"test_UBB_FINAL_TEST_RESULT_{timestamp}.csv"
 
 ITERS_PER_SAMPLE_SIZE = 20
 TEST_BOOTSTRAP_ITERS = 100
@@ -25,13 +27,23 @@ VERBOSE = True
 # ML_MODEL = LinearRegression()
 # PARAM_GRID = {}
 
-ML_MODEL = RandomForestRegressor()
+# Define the Decision Tree model and parameter grid
+ML_MODEL = DecisionTreeRegressor()
 PARAM_GRID = {
-    'n_estimators': [100],  
-    'max_depth': [None, 10],
-    'min_samples_split': [2, 20],
-    'min_samples_leaf': [1, 10]
+    'max_depth': [None, 10, 20, 30],  # Expanded range
+    'min_samples_split': [2, 10, 20],
+    'min_samples_leaf': [1, 5, 10],
+    'max_leaf_nodes': [None, 20],
+    'min_impurity_decrease': [0.0, 0.001, 0.01, 0.1]
 }
+
+# ML_MODEL = RandomForestRegressor()
+# PARAM_GRID = {
+#     'n_estimators': [100], #[100] ,
+#     'max_depth': [None, 10],
+#     'min_samples_split': [2, 20],
+#     'min_samples_leaf': [1, 20]
+# }
 
 DATA_SOURCE = "../data/simulation/"
 
@@ -71,7 +83,7 @@ def process_iteration(params):
 
 def main():
     true_models = ["UBB"]
-    effective_sample_sizes = [5000] #[500, 1000, 2000, 3000, 4000, 5000]
+    effective_sample_sizes = [3000, 4000, 5000] #[500, 1000, 2000, 3000, 4000, 5000]
 
     columns = ['true_model', 'data_source', 'network_size', 'effective_sample_size',
                'test_bootstrap_iters', 'ML_model_name', 'tuning_param_grid',
