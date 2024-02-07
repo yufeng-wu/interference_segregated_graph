23502c9561da14f15d54d0892023693d7cce4a68
Yufeng Wu <sw20@williams.edu>
2024-02-06 19:55:48 -0500

    test for 6\7000

--- main/BBU_parallelized.py
+++ main/BBU_parallelized.py
@@ -85,7 +85,7 @@ def process_iteration(params):
 
 def main():
     true_models = ["BBU"] 
-    effective_sample_sizes = [2000, 3000, 4000, 5000]
+    effective_sample_sizes = [6000, 7000] #[2000, 3000, 4000, 5000]
 
     columns = ['true_model', 'data_source', 'network_size', 'effective_sample_size',
                'test_bootstrap_iters', 'ML_model_name', 'tuning_param_grid',
