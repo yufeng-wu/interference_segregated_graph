7120bffe2db63e026211b4bc65ce2ca155f3e525
Yufeng Wu <sw20@williams.edu>
2024-02-15 23:40:04 -0500

    exp fixed L

--- main/autog.py
+++ main/autog.py
@@ -488,7 +488,7 @@ def run_experiment(num_subjects):
     conf_int = bootstrap_confidence_interval(
         data=est_df, n_bootstraps=300, alpha=0.05, n_samples_autog=N_SAMPLES, 
         burn_in_autog=BURN_IN, network=network, num_of_subejects=NUM_OF_SUBJECTS, 
-        A_val=1, L_sample=None)
+        A_val=1, L_sample=GM_sample['L'])
     
     print("AUTO-G Confidence Interval:", conf_int)
 
@@ -503,7 +503,7 @@ def run_experiment(num_subjects):
 
 def run_experiments_with_multiprocessing():
     num_subjects_list = [500, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000]
-    output_file = 'AUTOG_EXP_RESULTS.csv'
+    output_file = 'AUTOG_FIXED_L_EXP_RESULTS.csv'
     
     pd.DataFrame([], columns=['num_subjects', 'true_beta_alpha', 'confidence_interval']).to_csv(output_file, index=False)
 
@@ -514,7 +514,7 @@ def run_experiments_with_multiprocessing():
             pd.DataFrame([result]).to_csv(output_file, mode='a', index=False, header=False)
     
     print(f"Experiment completed. Results saved to '{output_file}'.")
-    
+
 
 def main():
     run_experiments_with_multiprocessing()
