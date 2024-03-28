ad52075b2428b8e2d2abbe8eb114911c6a54d559
Yufeng Wu <sw20@williams.edu>
2024-03-28 10:38:39 -0400

    new sim:

--- main/simulation/UUB_autog.py
+++ main/simulation/UUB_autog.py
@@ -10,12 +10,12 @@ L_EDGE_TYPE = 'U'
 A_EDGE_TYPE = 'U'
 Y_EDGE_TYPE = 'B'
 
-TRUE_CAUSAL_EFFECT_N_UNIT = 10000
-AVG_DEGREE = 6
-N_UNITS_LIST = [1000, 2000, 5000, 8000]
+TRUE_CAUSAL_EFFECT_N_UNIT = 20000
+AVG_DEGREE = 5
+N_UNITS_LIST = [10000, 15000, 20000]
 N_ESTIMATES = 15 # number of causal effect estimates for each n_unit
-N_SIMULATIONS = 100 # the number of L samples to draw 
-BURN_IN = 200
+N_SIMULATIONS = 50 # the number of L samples to draw 
+BURN_IN = 150 #200
 
 # true parameters of the Data Generating Process
 L_TRUE = np.array([-0.3, 0.4])
