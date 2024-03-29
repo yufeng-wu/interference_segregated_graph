5999c13df14acde102b5bcf24180af35c9edb14e
Yufeng Wu <sw20@williams.edu>
2024-03-29 10:25:35 -0400

    more test

--- main/simulation/UUB_autog.py
+++ main/simulation/UUB_autog.py
@@ -11,9 +11,9 @@ L_EDGE_TYPE = 'U'
 A_EDGE_TYPE = 'U'
 Y_EDGE_TYPE = 'B'
 
-TRUE_CAUSAL_EFFECT_N_UNIT = 10000
+TRUE_CAUSAL_EFFECT_N_UNIT = 20000
 AVG_DEGREE = 5
-N_UNITS_LIST = [1000, 3000, 5000, 7000, 9000]
+N_UNITS_LIST = [11000, 13000, 15000]
 N_ESTIMATES = 100 # number of causal effect estimates for each n_unit
 N_SIMULATIONS = 100 # the number of L samples to draw 
 BURN_IN = 200
