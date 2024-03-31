c8cab64f5d864aef6b5c45f4f1e687f8d2759da9
Yufeng Wu <sw20@williams.edu>
2024-03-31 15:24:00 -0400

    new dgp

--- main/simulation/UBB_ours.py
+++ main/simulation/UBB_ours.py
@@ -25,14 +25,14 @@ BURN_IN = 200
 
 # true parameters of the Data Generating Process
 L_TRUE = np.array([-0.3, 0.4])
-A_TRUE = np.array([0, 1, 0.3, -0.4, -0.7, 0.2])
+A_TRUE = np.array([2, 1, 1.3, -0.4, -0.7, 0.2])
 Y_TRUE = np.array([0, 1, -3, 0.1, 1, -0.3, 1, 2])
 
 
 def parallel_helper(n_units):
     network_dict, network_adj_mat = create_random_network(n_units, AVG_DEGREE)
     L, A, Y = sample_LAY(network_adj_mat, L_EDGE_TYPE, A_EDGE_TYPE, Y_EDGE_TYPE, L_TRUE, A_TRUE, Y_TRUE, BURN_IN)
-
+    print(np.mean(L), np.mean(A), np.mean(Y))
     return estimate_causal_effects_U_B(network_dict, network_adj_mat, L, A, Y, 
                                        N_SIMULATIONS, gibbs_select_every=3, 
                                        burn_in=BURN_IN)
@@ -41,9 +41,9 @@ def parallel_helper(n_units):
 def main():
     
     ''' evaluate true network causal effects '''
-    _, network_adj_mat = create_random_network(TRUE_CAUSAL_EFFECT_N_UNIT, AVG_DEGREE)
-    causal_effect_true = true_causal_effects_U_B(network_adj_mat, L_TRUE, Y_TRUE, BURN_IN, N_SIMULATIONS)
-    
+    # _, network_adj_mat = create_random_network(TRUE_CAUSAL_EFFECT_N_UNIT, AVG_DEGREE)
+    # causal_effect_true = true_causal_effects_U_B(network_adj_mat, L_TRUE, Y_TRUE, BURN_IN, N_SIMULATIONS)
+    causal_effect_true = 0.437
     print("True causal effect:", causal_effect_true)
     
     ''' using autog to estimate causal effects from data generated from UBB '''
