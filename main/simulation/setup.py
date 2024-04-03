9d76edd322c75f013ec39eed53137d3aad0eb913
Yufeng Wu <sw20@williams.edu>
2024-04-03 10:17:54 -0400

    new exp

--- main/simulation/setup.py
+++ main/simulation/setup.py
@@ -7,11 +7,11 @@ from our_estimation_methods import *
 warnings.filterwarnings("ignore", category=FutureWarning)
 
 # the common set up of my tests
-TRUE_CAUSAL_EFFECT_N_UNIT = 15000 
+TRUE_CAUSAL_EFFECT_N_UNIT = 3000 
 AVG_DEGREE = 5
-N_UNITS_LIST = [7000, 10000, 15000]
-N_ESTIMATES = 80 # number of causal effect estimates for each n_unit
-N_SIMULATIONS = 100 # the number of L samples to draw 
+N_UNITS_LIST = [500, 1000, 2000]
+N_ESTIMATES = 50 # number of causal effect estimates for each n_unit
+N_SIMULATIONS = 1000 # the number of L samples to draw 
 BURN_IN = 200
 GIBBS_SELECT_EVERY = 3
 # TRUE_CAUSAL_EFFECT_N_UNIT = 2000 
--- main/simulation/visualizer.py
+++ main/simulation/visualizer.py
@@ -11,9 +11,9 @@ n_samples = [int(column_name.split()[2]) for column_name in df.columns if 'n uni
 plt.figure(figsize=(10, 6))
 for n_sample in n_samples:
     # add some jitter
-    x_vals = np.random.normal(n_sample, 10, len(df[f'n units {n_sample}']))
-    plt.plot(x_vals, df[f'n units {n_sample}'], 'ro', markersize=2)
-    # plt.boxplot(df[f'n units {n_sample}'], positions=[n_sample], widths=200)
+    x_vals = np.random.normal(n_sample, 100, len(df[f'n units {n_sample}']))
+    #plt.plot(x_vals, df[f'n units {n_sample}'], 'ro', markersize=2)
+    plt.boxplot(df[f'n units {n_sample}'], positions=[n_sample], widths=200)
 
 # also add a line for the true causal effect
 plt.axhline(y=df['True Effect'][0], color='r', linestyle='--')
@@ -23,4 +23,5 @@ plt.ylabel('Causal Effect')
 plt.title(f'Causal Effect Estimates {filename}')
 # plt.show()
 
-plt.savefig(f'./result{dirname}/{filename}.png')
+# plt.savefig(f'./result{dirname}/figures/{filename}.png')
+plt.savefig(f'./result/{filename}.png')
