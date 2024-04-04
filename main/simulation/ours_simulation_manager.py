f3e384636176f0143503596de9948ed6611026b6
Yufeng Wu <sw20@williams.edu>
2024-04-04 00:01:11 -0400

    yay

--- main/simulation/ours_simulation_manager.py
+++ main/simulation/ours_simulation_manager.py
@@ -4,6 +4,9 @@ import subprocess
 files_in_directory = os.listdir()
 experiments = [f for f in files_in_directory if f.endswith("_ours.py")]
 
+# sort the experiments by name
+experiments.sort()
+
 for experiment in experiments:
     print(f"Running {experiment}...")
     subprocess.call(['python3', experiment])
