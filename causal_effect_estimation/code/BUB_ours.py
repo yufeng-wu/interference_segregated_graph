02db23a4ce55b7bfdce60f01c80a240a59a4bb18
Yufeng Wu <sw20@williams.edu>
2024-04-09 09:39:58 -0400

    updated DGP

--- causal_effect_estimation/code/BUB_ours.py
+++ causal_effect_estimation/code/BUB_ours.py
@@ -14,7 +14,7 @@ def parallel_helper(n_units):
     # note: the parameter burn_in won't be used since all layers are bidirected.
     L, A, Y = sample_LAY(network_adj_mat, L_EDGE_TYPE, A_EDGE_TYPE, Y_EDGE_TYPE, 
                          L_TRUE, A_TRUE, Y_TRUE, BURN_IN)
-
+    # print(np.mean(L), np.mean(A), np.mean(Y))
     return estimate_causal_effects_B_B(network_dict, network_adj_mat, L, A, Y, 
                                        MAX_NEIGHBORS, int(N_SIM_MULTIPLIER*n_units))
         
--- causal_effect_estimation/code/visualizer.py
+++ causal_effect_estimation/code/visualizer.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-filename = "BUB_ours"
+filename = "UUB_ours"
 
 df = pd.read_csv(f'../result/raw_output/{filename}.csv')
 n_samples = [int(column_name.split()[2]) for column_name in df.columns if 'n units' in column_name]
@@ -10,13 +10,15 @@ n_samples = [int(column_name.split()[2]) for column_name in df.columns if 'n uni
 plt.figure(figsize=(10, 6))
 for n_sample in n_samples:
     # add some jitter
-    # x_vals = np.random.normal(n_sample, 100, len(df[f'n units {n_sample}']))
+    x_vals = np.random.normal(n_sample, 100, len(df[f'n units {n_sample}']))
     # plt.plot(x_vals, df[f'n units {n_sample}'], 'ro', markersize=2, alpha=0.1)
     
-    plt.boxplot(df[f'n units {n_sample}'], positions=[n_sample], widths=200, showfliers=False)
+    plt.boxplot(df[f'n units {n_sample}'], positions=[n_sample], widths=200)
 
 # add a line for the true causal effect
 plt.axhline(y=df['True Effect'][0], color='r', linestyle='--')
+
+# plt.ylim(0, 0.5)
 plt.xlabel('Number of units')
 plt.ylabel('Causal Effect')
 plt.title(f'Causal Effect Estimates {filename}')
