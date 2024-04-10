04d838ce7e22574456d31f48860e85294d113a9b
Yufeng Wu <sw20@williams.edu>
2024-04-09 20:14:02 -0400

    now test again

--- causal_effect_estimation/code/visualizer.py
+++ causal_effect_estimation/code/visualizer.py
@@ -13,7 +13,7 @@ for n_sample in n_samples:
     x_vals = np.random.normal(n_sample, 100, len(df[f'n units {n_sample}']))
     plt.plot(x_vals, df[f'n units {n_sample}'], 'ro', markersize=2, alpha=0.5)
     
-    plt.boxplot(df[f'n units {n_sample}'], positions=[n_sample], widths=200)
+    #plt.boxplot(df[f'n units {n_sample}'], positions=[n_sample], widths=200)
 
 # add a line for the true causal effect
 plt.axhline(y=df['True Effect'][0], color='r', linestyle='--')
